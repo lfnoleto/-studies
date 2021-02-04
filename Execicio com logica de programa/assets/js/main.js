803b18a03a9c6e73282a2d211410b6d7bd93248e
Lucas Ferreira Noleto <lucasnoleto18@gmail.com>
2021-02-03 22:29:00 -0300

    feat: criação do execicios maior valor

--- Execicio com logica de programa/assets/js/main.js
+++ Execicio com logica de programa/assets/js/main.js
@@ -1,3 +1,15 @@
+let valor1 = 40
+let valor2 = 20
 
 
+function MaiorValor(valor,valor2){
+
+  if (valor>valor2)
+    return valor
+  if(valor2>valor1)
+    return valor2
+}
+
+
+console.log(MaiorValor(valor1,valor2))
 
